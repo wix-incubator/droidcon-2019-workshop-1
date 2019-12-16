4e39fcb6782d6075358a5e2786633844e6800846
Benny Holtzer <bennyh@wix.com>
2019-12-16 16:25:50 +0200

    First Commit

---
+++ src/CharacterListItem.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {View, Card, Button, Text} from 'react-native-ui-lib';
+
+const deleteIcon = require('../img/delete.png');
+const favorite = require('../img/favorite.png');
+const favoriteSelected = require('../img/favorite_selected.png');
+
+export default class CharacterListItem extends React.PureComponent {
+  render() {
+    let img =
+      this.props.detail.thumbnail.path +
+      '/portrait_medium.' +
+      this.props.detail.thumbnail.extension;
+    let name = this.props.detail.name;
+    let desc = this.props.detail.description
+      ? this.props.detail.description
+      : 'no description';
+    return (
+      <Card row height={150} style={styles.card}>
+        <Card.Image width={100} imageSource={{uri: img}} />
+        <View padding-20 flex>
+          <Text text70 white>
+            {name}
+          </Text>
+          <Text
+            style={styles.testDesc}
+            text90
+            dark50
+            numberOfLines={4}
+            ellipsizeMode={'tail'}>
+            {desc}
+          </Text>
+          <View row style={styles.btn}>
+            <Button
+              text90
+              link
+              red30
+              iconSource={deleteIcon}
+              label="Remove"
+              onPress={() => this.props.onRemoveItem(this.props.detail.id)}
+            />
+          </View>
+        </View>
+      </Card>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  card: {
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor: '#3a3535',
+  },
+  testDesc: {
+    height: 60,
+    marginBottom: 15,
+  },
+  btn: {
+    alignSelf: 'flex-end',
+  },
+});
